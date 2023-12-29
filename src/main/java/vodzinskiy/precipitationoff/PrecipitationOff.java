36fc1b17d006a78fd5864b269ce2ad85ee19da1b
Vodzinskiy <roma.vodzinskiy@gmail.com>
2023-12-29 17:45:43 +0200

    init project

---
+++ src/main/java/vodzinskiy/precipitationoff/PrecipitationOff.java
@@ -0,0 +1,17 @@
+package vodzinskiy.precipitationoff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrecipitationOff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
