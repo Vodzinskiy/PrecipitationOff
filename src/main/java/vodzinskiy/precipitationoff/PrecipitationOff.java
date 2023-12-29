1b8a257bd5be232e622becc645cf82d3f2f531ae
Vodzinskiy <roma.vodzinskiy@gmail.com>
2023-12-29 20:34:09 +0200

    area-list file creation

--- src/main/java/vodzinskiy/precipitationoff/PrecipitationOff.java
+++ src/main/java/vodzinskiy/precipitationoff/PrecipitationOff.java
@@ -1,13 +1,29 @@
 package vodzinskiy.precipitationoff;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
+import vodzinskiy.precipitationoff.commands.Select;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 
 public final class PrecipitationOff extends JavaPlugin {
+    private  static  final Logger logger = LogManager.getLogger("PrecipitationOff");
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        try {
+            Path path = Path.of("./plugins/PrecipitationOff");
+            Files.createDirectory(path);
+            Path yamlPath = path.resolve("area-list.yaml");
+            Files.newBufferedWriter(yamlPath);
+            logger.info("\"list.yaml\" created");
+        } catch (Exception ignored) {}
 
+        Objects.requireNonNull(this.getCommand("select")).setExecutor(new Select());
+        logger.info("Enabled");
     }
 
     @Override
---
+++ src/main/java/vodzinskiy/precipitationoff/commands/Select.java
@@ -0,0 +1,17 @@
+package vodzinskiy.precipitationoff.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class Select implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (commandSender instanceof Player) {
+            commandSender.sendMessage("select");
+        }
+        return true;
+    }
+}
